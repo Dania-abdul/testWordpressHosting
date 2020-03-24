98ec4ff1afba7e031f9a931ce15fd272a7c78444
Dania Abdul <43537934+Dania-abdul@users.noreply.github.com>
2020-03-24 15:15:43 +0100

    add about

--- wp-content/themes/customtheme/front-page.php
+++ wp-content/themes/customtheme/front-page.php
@@ -14,7 +14,17 @@
     <div class="intro">
         <h2 class="intro__title">Wordpress custom page and Blog</h2>
         <p class="intro__text">This is a completely responsive website built from scratch using WordPress, HTML, CSS, JavaScpript, and PHP</p>
-        <a href="<?php echo site_url('/about'); ?>" class="btn-custom">About</a>
+        <div class="about">
+            <h2>About me</h2>
+            <p class="about-info">Hello my name is dania </p>
+            <p class="about-info">I am a front-end developer I enjoy building websites </p>
+            <p class="about-info">please visit my portfolio</p>
+            <ul class="social">
+                <li class="social__item"><a href="https://dania-abdul.github.io/">github</a></li>
+                <li class="social__item"><a href="https://www.linkedin.com/in/dania-abdul/">linkedin</a></li>
+            </ul>
+
+        </div>                              
 
     </div>
         
